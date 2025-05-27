477ad3e79ec5f98dc347d4cc2476d24c4c51cfdd
Yiming-Liang <liangyiming221@mails.ucas.ac.cn>
2025-05-27 04:55:54 +0000

    add rollout 1 sh

---
+++ examples/run_align_infer_1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 设置项目根目录
+PROJECT_ROOT=$(dirname $(dirname $(realpath $0)))
+echo "项目根目录: $PROJECT_ROOT"
+# 进入项目根目录
+cd $PROJECT_ROOT
+
+# 创建输出目录
+mkdir -p results
+
+#### 1. 运行前需修改main函数的config文件地址 （默认是configs/align/simple_qa.yaml） ####
+# 简单的QA推理
+python main.py --config-name simple_qa \
+    inference.rollout_num=1 \
+    data.output_file=results/align/align_3_convertor_back_qa_test_rollout1.json 
+    # data.num_samples=32
+###################### end 1 ###############################
+
+
+#### 2. 运行前需修改main函数的config文件地址 （默认是configs/align/score_checklist.yaml） ####
+python main.py --config-name score_checklist \
+    data.input_file=results/align/align_3_convertor_back_qa_test_rollout1.json \
+    data.output_file=results/align/align_3_convertor_back_qa_test_rollout1_checklist_score.json \
+    inference.rollout_num=1 \
+    processor.post_process=[extract_score,average_max_score]
+    # data.num_samples=32
+###################### end 2 ###############################
+
+#### 3. 运行前需修改main函数的config文件地址 （默认是configs/align/score_effectlist.yaml） ####
+# python main.py
+###################### end 3 ###############################
+
+
+
